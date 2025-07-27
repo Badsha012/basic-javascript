7eda9c57a0d4213af050c688c0fa7dd75d5e3dda
Badsha012 <badshasheikh361@gmail.com>
2025-07-26 23:31:45 -0700

    seven commit

---
+++ naming.js
@@ -0,0 +1,26 @@
+// 1.no keyword in variable
+
+ var false=45;
+ var var =45;
+
+// 2.no space or gap in variable  name
+ var  ishappy=true;
+ var ishappy=false;
+
+// 3.no quote
+ var "address"="kochu khet"
+
+// 4.canton strat with a number other than first letter is allowed
+ var 3money=33;
+
+
+
+// 5.name is case sensitive
+  Address
+
+// 6.how to write a log variable name
+var my_current_address="andrkikls "  //snake case
+
+ var mycurrentaddress="andrkikls "
+  var myCurrentAddress="andrkikls "  //camel case
+var Mycurrentaddress="andrkikls "
