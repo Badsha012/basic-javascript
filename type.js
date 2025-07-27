0c15913fdfb40bdf41955a68acb8b67effc40c21
Badsha012 <badshasheikh361@gmail.com>
2025-07-26 23:12:38 -0700

    Five commit

--- type.js
+++ type.js
@@ -3,8 +3,15 @@
 
 var name="Mim";
 var country='Bangladesh';
-console.log(name);
+console.log( typeof name);
 
-// boolean
+// boolean type
 var isRich=true;
+var isPoor=false
 console.log(isRich);
+// 
+console.log( typeof isPoor)
+console.log(country);
+var price=1200;
+console.log(typeof price);
+
