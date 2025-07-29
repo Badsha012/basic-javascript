59e9388a3cc98e90b182d92c73b14baf43ce9020
Badsha012 <badshasheikh361@gmail.com>
2025-07-29 09:32:57 -0700

    nine commit

--- .vscode/diff.js
+++ .vscode/diff.js
@@ -14,7 +14,20 @@
 
 /// give me the list number between 1 to 30 divisible 5
 for(let i=1;i<=30;i++){
-    if(i % 5===0){
+    if(i % 3===0 || i%5===0){
         console.log(i);
     }
 }
+
+
+
+/// give me  sum of nmber from 1 to 20 that are are divisible by 3
+
+let sum=0;
+for(let i=1;i<=20;i++){
+    if(i % 3===0){
+        // console.log(i);
+        sum=sum+i;
+    }
+}
+    console.log('total ofnumber:',sum);
