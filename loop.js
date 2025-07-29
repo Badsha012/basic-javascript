ae41ff1c7d18e686b5e85aa8f2e080bd44c3fcf3
Badsha012 <badshasheikh361@gmail.com>
2025-07-29 07:20:03 -0700

    three commit

---
+++ loop.js
@@ -0,0 +1,23 @@
+
+//const:value wil not change
+//var :we hould not use it
+//let:value change
+
+
+//+++++++++++++  ARRAY   ++++++++++
+const number=[12,1,14,15,16,17];
+console.log(number);
+// +++++++++++++  LOOOP   +++++++++
+for( const num of number){
+    console.log(num);
+}
+
+
+const fruits=['apple','mango'];
+
+
+for( const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat');
+
+}
