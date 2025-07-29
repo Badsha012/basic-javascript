1f7404e15cab860299eac36b0cb146bf3cde4845
Badsha012 <badshasheikh361@gmail.com>
2025-07-29 09:25:36 -0700

    seven commit

--- .vscode/dc_while.js
+++ .vscode/dc_while.js
@@ -5,5 +5,7 @@ let x=10;
 while(x>=0){
     console.log(x);
 
+    x--;
+
 }
     
---
+++ .vscode/diff.js
@@ -0,0 +1,20 @@
+
+
+// DIFFERENT WAYS TO USE LOOP
+
+
+
+// for(let i=1;i<=20;i++){
+//     if(i%2==1)
+//     {
+//         console.log('odd',i);
+//     }
+// }
+
+
+/// give me the list number between 1 to 30 divisible 5
+for(let i=1;i<=30;i++){
+    if(i % 5===0){
+        console.log(i);
+    }
+}
