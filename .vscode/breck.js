ad7664094232980a7594126d422cb770a7e20e06
Badsha012 <badshasheikh361@gmail.com>
2025-07-29 21:29:38 -0700

    five commit

--- .vscode/breck.js
+++ .vscode/breck.js
@@ -6,3 +6,5 @@ for(let i=0;i<=10;i++){
         break;
     }
 }
+
+
---
+++ .vscode/breck_while.js
@@ -0,0 +1,11 @@
+
+let i=54;
+while(i>25){
+    console.log(i);
+    if(i<50){
+        break;
+    }
+
+     console.log(i);
+    i--;
+}
