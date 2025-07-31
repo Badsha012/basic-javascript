b474e1a37c4aaa8d5f71c6dace54cf93b69d3143
Badsha012 <badshasheikh361@gmail.com>
2025-07-31 02:40:26 -0700

    nine commit

---
+++ .vscode/incude.js
@@ -0,0 +1,11 @@
+const friends=['mim','dim','cim','vim'];
+console.log(friends.includes('vim'));
+console.log(friends.includes('jan'));
+
+
+if(friends.includes('mim')){
+    console.log('i love u');
+}
+else{
+    console.log('I heat you');
+}
---
+++ .vscode/indx.js
@@ -0,0 +1,10 @@
+const friends=['mim','dim','cim','vim'];
+console.log(friends.indexOf('cim'));
+
+
+console.log(friends.indexOf('suma'));
+
+
+const food=' ros o gol l a';
+console.log( Array.isArray(food));
+console.log(Array .isArray(friends));
